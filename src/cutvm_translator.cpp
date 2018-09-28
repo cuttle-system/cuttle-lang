28fa5a1ff30aa24a709fa8464d0ae074de993f38
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-09-28 11:38:18 +0300

    update cutvm_translator

--- src/cutvm_translator.cpp
+++ src/cutvm_translator.cpp
@@ -21,55 +21,61 @@ const char *cutvm_type_from_value_type(const value_type &value_type) {
 }
 
 void lang::get_cutvm_translator(translator_t &translator) {
-    dictionary_t dictionary;
+    translator = {{TRANSLATOR_ANY_NAME, TRANSLATOR_ANY_VERSION}, {"cutvm", 1}, {}};
+    initialize(translator.dictionary);
 
-    add(dictionary, TRANSLATOR_ANY_NAME, DICTIONARY_ANY_ARG_NUMBER, [](translate_state_t &state) {
-        namespace df = dictionary_funcs;
+    add(translator.dictionary, call_tree_t{{{}, {0}}},
+        tokens_t{token{token_type::macro_pf, "_func"}},
+        [](translate_state_t &state) {
+            namespace df = dictionary_funcs;
 
-        std::vector<unsigned int> args_indexes;
+            std::vector<unsigned int> args_indexes;
 
-        unsigned int call_id;
-        if (state.index_reference.find(state.index) != state.index_reference.end()) {
-            call_id = state.index_reference[state.index];
-            state.values[call_id] = {"call", value_type::func_name};
-        } else {
-            call_id = df::function_name(state, "call");
-        }
-
-        auto b_id = df::function_name(state, "b");
-        auto s_id = df::function_name(state, "s");
-        args_indexes.push_back(df::function(state, b_id, {
-                df::function(state, s_id, {
-                        df::value(state, state.tokens[state.index].value, value_type::string)
-                })
-        }));
-
-        for (auto arg_index : state.tree.src[state.index]) {
-            auto token_value_type = value_from_token_type(state.tokens[arg_index].type);
-            auto arg_b_id = df::function_name(state, "b");
-            if (token_value_type != value_type::func_name) {
-                auto type_id = df::function_name(state, cutvm_type_from_value_type(token_value_type));
-                df::function(state, arg_b_id, {
-                        df::function(state, type_id, {
-                                df::value(state, state.tokens[arg_index].value, token_value_type)
-                        })
-                });
+            unsigned int call_id;
+            if (state.index_reference.find(state.index) != state.index_reference.end()) {
+                call_id = state.index_reference[state.index];
+                state.values[call_id] = {"call", value_type::func_name};
+            } else {
+                call_id = df::function_name(state, "call");
             }
-            state.index_reference[arg_index] = arg_b_id;
-            args_indexes.push_back(arg_b_id);
-        }
 
-        auto c_id = df::function_name(state, "c");
-        args_indexes.push_back(df::function(state, c_id, {
-                df::number(state, "0"),
-                df::number(state, std::to_string(args_indexes.size())),
-                df::function(state, df::function_name(state, "array"), {})
-        }));
+            auto b_id = df::function_name(state, "b");
+            auto s_id = df::function_name(state, "s");
+            args_indexes.push_back(df::function(state, b_id, {
+                    df::function(state, s_id, {
+                            df::value(state, state.tokens[state.index].value, value_type::string)
+                    })
+            }));
 
-        df::function(state, call_id, args_indexes);
+            for (auto arg_index : state.tree.src[state.index]) {
+                auto token_value_type = value_from_token_type(state.tokens[arg_index].type);
+                tree_src_element_t arg_b_id;
+                if (token_value_type != value_type::func_name) {
+                    arg_b_id = df::function_name(state, "b");
+                    auto type_id = df::function_name(state, cutvm_type_from_value_type(token_value_type));
+                    df::function(state, arg_b_id, {
+                            df::function(state, type_id, {
+                                    df::value(state, state.tokens[arg_index].value, token_value_type)
+                            })
+                    });
+                } else {
+                    translate_state_t child_state = state;
+                    child_state.index = arg_index;
+                    arg_b_id = translate_function_call(child_state);
+                }
+                state.index_reference[arg_index] = arg_b_id;
+                args_indexes.push_back(arg_b_id);
+            }
 
-        return call_id;
-    });
+            auto c_id = df::function_name(state, "c");
+            args_indexes.push_back(df::function(state, c_id, {
+                    df::number(state, "0"),
+                    df::number(state, std::to_string(args_indexes.size())),
+                    df::function(state, df::function_name(state, "array"), {})
+            }));
 
-    translator = { { TRANSLATOR_ANY_NAME, TRANSLATOR_ANY_VERSION }, { "cutvm", 1 }, dictionary };
+            df::function(state, call_id, args_indexes);
+
+            return call_id;
+        });
 }
--- tests/test_cutvm_translator.cpp
+++ tests/test_cutvm_translator.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <boost/test/unit_test.hpp>
 #include <string>
+#include <unsupported_cutvm_type_error.hpp>
+#include <dictionary_methods.hpp>
+#include <dictionary_funcs.hpp>
 #include "translator_methods.hpp"
 #include "cutvm_translator.hpp"
 #include "value_methods.hpp"
@@ -50,28 +53,31 @@ BOOST_FIXTURE_TEST_SUITE(can_translate_basic_function_calls_suite, cutvm_transla
     }
 
     BOOST_AUTO_TEST_CASE(case2) {
+        values_t values;
+        call_tree_t new_tree;
         tokens_t tokens = {
-                { token_type::number, "1", 0, 0 },
-                { token_type::atom, "+", 0, 0 },
-                { token_type::number, "2", 0, 0 },
-                { token_type::atom, "-", 0, 0 },
-                { token_type::number, "2", 0, 0 }
+                {token_type::number, "1", 0, 0},
+                {token_type::atom,   "+", 0, 0},
+                {token_type::number, "2", 0, 0},
+                {token_type::atom,   "-", 0, 0},
+                {token_type::number, "2", 0, 0}
         };
-        call_tree_t tree = { {
-                                     {}, {0, 3},
-                                     {}, {2, 4}, {}, {1}
-                             } };
+        call_tree_t tree = {{
+                                    {}, {0, 3},
+                                    {}, {2, 4}, {}, {1}
+                            }};
         translate(translator, tokens, tree, values, new_tree);
         BOOST_CHECK(new_tree.src == (tree_src_t{
-                {1, 4, 7, 8},
+                {1, 4, 7, 21},
                 {2}, {3}, {},
                 {5}, {6}, {},
-                {12, 15, 18, 21},
-                {9, 10, 11}, {}, {}, {},
 
-                {13}, {14}, {},
-                {16}, {17}, {},
-                {19}, {20}, {},
+                {8, 11, 14, 17},
+                {9}, {10}, {},
+                {12}, {13}, {},
+                {15}, {16}, {},
+                {18, 19, 20}, {}, {}, {},
+
                 {22, 23, 24}, {}, {}, {},
                 {0}
         }));
@@ -80,11 +86,10 @@ BOOST_FIXTURE_TEST_SUITE(can_translate_basic_function_calls_suite, cutvm_transla
                 {"b", value_type::func_name}, {"s", value_type::func_name}, {"+", value_type::string},
                 {"b", value_type::func_name}, {"i", value_type::func_name}, {"1", value_type::number},
                 {"call", value_type::func_name},
-                {"c", value_type::func_name}, {"0", value_type::number}, {"3", value_type::number}, {"array", value_type::func_name},
-
                 {"b", value_type::func_name}, {"s", value_type::func_name}, {"-", value_type::string},
                 {"b", value_type::func_name}, {"i", value_type::func_name}, {"2", value_type::number},
                 {"b", value_type::func_name}, {"i", value_type::func_name}, {"2", value_type::number},
+                {"c", value_type::func_name}, {"0", value_type::number}, {"3", value_type::number}, {"array", value_type::func_name},
                 {"c", value_type::func_name}, {"0", value_type::number}, {"3", value_type::number}, {"array", value_type::func_name}
         }));
     }
