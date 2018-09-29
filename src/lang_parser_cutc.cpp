aa8a2abecfd2c44e39abe6db039d80be9aadb4d1
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-09-29 23:28:56 +0300

    add more functions

--- src/lang_parser_cutc.cpp
+++ src/lang_parser_cutc.cpp
@@ -8,4 +8,5 @@ void lang::get_parser_cutc(context_t &context) {
     get_parser_base(context);
     add(context, ".", function_t{ function_type::infix, 2 }, 3);
     add(context, "to", function_t{ function_type::infix, 2 }, 4);
+    add(context, "just", function_t{ function_type::prefix, 1 }, 4);
 }
--- src/lang_parser_cutc_tokenizer.cpp
+++ src/lang_parser_cutc_tokenizer.cpp
@@ -8,4 +8,9 @@ void lang::get_parser_cutc_tokenizer(context_t &context) {
     get_parser_base(context);
     add(context, "->", function_t{ function_type::infix, 2 }, 3);
     add(context, "normal_string", function_t{ function_type::prefix, 1 }, 4);
+    add(context, "formatted_string", function_t{ function_type::prefix, 1 }, 4);
+    add(context, "comments", function_t{ function_type::prefix, 1 }, 4);
+    add(context, "macro_ps", function_t{ function_type::prefix, 1 }, 4);
+    add(context, "macro_pf", function_t{ function_type::prefix, 1 }, 4);
+    add(context, "macro_p", function_t{ function_type::prefix, 1 }, 4);
 }
