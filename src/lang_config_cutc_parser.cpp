9cedad67cbc851276693a0b9e323665a322f06e9
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-10-14 21:53:32 +0300

    fix lang config priority

--- src/lang_config_cutc_parser.cpp
+++ src/lang_config_cutc_parser.cpp
@@ -7,8 +7,8 @@ using namespace cuttle;
 void lang::get_config_cutc_parser(context_t &context) {
     get_parser_base(context);
 
+    add(context, ".", function_t{function_type::infix, 2}, FUNCTION_ID_UNKNOWN);
+
     add(context, "just", function_t{function_type::prefix, 1}, FUNCTION_ID_UNKNOWN);
     add(context, "to", function_t{function_type::infix, 2}, FUNCTION_ID_UNKNOWN);
-
-    add(context, ".", function_t{function_type::infix, 2}, FUNCTION_ID_UNKNOWN);
 }
