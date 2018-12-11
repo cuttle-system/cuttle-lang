50d98c52a0e865892a22332e125cd5f09d03eaf3
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-12-11 21:16:30 +0200

    update dependency manager

--- scripts/deps/cuttle-generator.sh
+++ scripts/deps/cuttle-generator.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-git clone git@github.com:cuttle-system/cuttle-generator.git
-cuttle-generator/scripts/get-deps.sh
+if [[ "$1" = "dev" ]]; then
+    git clone git@github.com:cuttle-system/cuttle-generator.git
+else
+    git clone https://github.com/cuttle-system/cuttle-generator.git
+fi
+bash cuttle-generator/scripts/get-deps.sh
--- scripts/deps/cuttle-translator.sh
+++ scripts/deps/cuttle-translator.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-git clone git@github.com:cuttle-system/cuttle-translator.git
-cuttle-translator/scripts/get-deps.sh
+if [[ "$1" = "dev" ]]; then
+    git clone git@github.com:cuttle-system/cuttle-translator.git
+else
+    git clone https://github.com/cuttle-system/cuttle-translator.git
+fi
+bash cuttle-translator/scripts/get-deps.sh
