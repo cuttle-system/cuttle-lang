1d8192203d86ebd2e14ae20596ece15d6c74dcda
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-12-29 16:35:24 +0200

    fix ./b2 bug and value_type name duplicate

--- src/cutvm_type_utils.cpp
+++ src/cutvm_type_utils.cpp
@@ -1,7 +1,8 @@
 #include "cutvm_type_utils.hpp"
+#include "value.hpp"
 
-const char *cuttle::cutvm_type_from_value_type(const value_type &value_type) {
-    switch (value_type) {
+const char *cuttle::cutvm_type_from_value_type(const value_type &type) {
+    switch (type) {
         case value_type::number:
             return "i";
         default:
