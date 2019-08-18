c51ff583b8a7db1007be94f34e6d312b0bf701d7
Michael Vietluzhskykh <mishavetl@gmail.com>
2019-08-18 09:14:07 +0300

    add parser -> cutvm translator test for plus function

--- tests/test_lang_parser_cutvm_translator.cpp
+++ tests/test_lang_parser_cutvm_translator.cpp
@@ -74,7 +74,7 @@ BOOST_FIXTURE_TEST_SUITE(lang_parser_can_translate_basic_rules_suite, lang_parse
                 {"b",                      value_type::func_name},
                 {"i",                      value_type::func_name},
                 {"0",                      value_type::number},
-                {"before",                   value_type::func_name},
+                {"before",                 value_type::func_name},
                 {"call",                   value_type::func_name},
                 {"b",                      value_type::func_name},
                 {"s",                      value_type::func_name},
@@ -135,7 +135,7 @@ BOOST_FIXTURE_TEST_SUITE(lang_parser_can_translate_basic_rules_suite, lang_parse
                 {"b",                      value_type::func_name},
                 {"i",                      value_type::func_name},
                 {"0",                      value_type::number},
-                {"root",                      value_type::func_name},
+                {"root",                   value_type::func_name},
                 {"call",                   value_type::func_name},
                 {"c",                      value_type::func_name},
                 {"0",                      value_type::number},
@@ -195,7 +195,7 @@ BOOST_FIXTURE_TEST_SUITE(lang_parser_can_translate_basic_rules_suite, lang_parse
                 {"b",                      value_type::func_name},
                 {"i",                      value_type::func_name},
                 {"2",                      value_type::number},
-                {"root",                      value_type::func_name},
+                {"root",                   value_type::func_name},
                 {"call",                   value_type::func_name},
                 {"c",                      value_type::func_name},
                 {"0",                      value_type::number},
@@ -259,11 +259,11 @@ BOOST_FIXTURE_TEST_SUITE(lang_parser_can_translate_basic_rules_suite, lang_parse
                 {"b",                      value_type::func_name},
                 {"i",                      value_type::func_name},
                 {"2",                      value_type::number},
-                {"before",                   value_type::func_name},
+                {"before",                 value_type::func_name},
                 {"call",                   value_type::func_name},
                 {"b",                      value_type::func_name},
                 {"s",                      value_type::func_name},
-                {"foo",                 value_type::string},
+                {"foo",                    value_type::string},
                 {"c",                      value_type::func_name},
                 {"1",                      value_type::number},
                 {"1",                      value_type::number},
@@ -291,9 +291,9 @@ BOOST_FIXTURE_TEST_SUITE(lang_parser_can_translate_basic_rules_suite, lang_parse
         };
         translate(translator, tokens, tree, values, new_tree);
         BOOST_CHECK(new_tree.src == (tree_src_t{
-                {2, 12, 15, 19, 26},
+                {2,  12, 15, 19, 26},
                 {},
-                {3, 6, 9},
+                {3,  6,  9},
                 {4},
                 {5},
                 {},
@@ -325,28 +325,28 @@ BOOST_FIXTURE_TEST_SUITE(lang_parser_can_translate_basic_rules_suite, lang_parse
         }));
         BOOST_CHECK(values == (values_t{
                 {"call",                   value_type::func_name},
-                {"->",                   value_type::func_name},
+                {"->",                     value_type::func_name},
                 {"call",                   value_type::func_name},
                 {"b",                      value_type::func_name},
                 {"s",                      value_type::func_name},
-                {"(",                 value_type::string},
+                {"(",                      value_type::string},
                 {"b",                      value_type::func_name},
                 {"s",                      value_type::func_name},
-                {")",                 value_type::string},
+                {")",                      value_type::string},
                 {"c",                      value_type::func_name},
                 {"2",                      value_type::number},
                 {"2",                      value_type::number},
                 {"b",                      value_type::func_name},
                 {"s",                      value_type::func_name},
-                {"postprefix",                 value_type::func_name},
+                {"postprefix",             value_type::func_name},
                 {"b",                      value_type::func_name},
                 {"i",                      value_type::func_name},
                 {"0",                      value_type::number},
-                {"before",                   value_type::func_name},
+                {"before",                 value_type::func_name},
                 {"call",                   value_type::func_name},
                 {"b",                      value_type::func_name},
                 {"s",                      value_type::func_name},
-                {"foo",                 value_type::string},
+                {"foo",                    value_type::string},
                 {"c",                      value_type::func_name},
                 {"1",                      value_type::number},
                 {"1",                      value_type::number},
@@ -357,48 +357,46 @@ BOOST_FIXTURE_TEST_SUITE(lang_parser_can_translate_basic_rules_suite, lang_parse
         }));
     }
 
-// TODO: implement type system for translator functions (return type, argument types, e.g.)
-
-//    BOOST_AUTO_TEST_CASE(case3) {
-//        tokens_t tokens = {
-//                {token_type::number, "1", 0, 0},
-//                {token_type::atom,   "+", 0, 0},
-//                {token_type::number, "2", 0, 0}
-//        };
-//        call_tree_t tree = {
-//                {
-//                        {}, {0, 2}, {}, {1}
-//                },
-//                {}
-//        };
-//        translate(translator, tokens, tree, values, new_tree);
-//        BOOST_CHECK(new_tree.src == (tree_src_t{
-//                {},
-//                {2, 5,  8},
-//                {3},
-//                {4},
-//                {},
-//                {6},
-//                {7},
-//                {},
-//                {9, 10, 0},
-//                {},
-//                {},
-//                {1}
-//        }));
-//        BOOST_CHECK(values == (values_t{
-//                {"+",    value_type::func_name},
-//                {"call", value_type::func_name},
-//                {"b",    value_type::func_name},
-//                {"i",    value_type::func_name},
-//                {"1",    value_type::number},
-//                {"b",    value_type::func_name},
-//                {"i",    value_type::func_name},
-//                {"2",    value_type::number},
-//                {"c",    value_type::func_name},
-//                {"2",    value_type::number},
-//                {"2",    value_type::number}
-//        }));
-//    }
+    BOOST_AUTO_TEST_CASE(case6) {
+        tokens_t tokens = {
+                {token_type::number, "1", 0, 0},
+                {token_type::atom,   "+", 0, 0},
+                {token_type::number, "2", 0, 0}
+        };
+        call_tree_t tree = {
+                {
+                        {}, {0, 2}, {}, {1}
+                },
+                {}
+        };
+        translate(translator, tokens, tree, values, new_tree);
+        BOOST_CHECK(new_tree.src == (tree_src_t{
+                {},
+                {2, 5,  8},
+                {3},
+                {4},
+                {},
+                {6},
+                {7},
+                {},
+                {9, 10, 0},
+                {},
+                {},
+                {1}
+        }));
+        BOOST_CHECK(values == (values_t{
+                {"+",    value_type::func_name},
+                {"call", value_type::func_name},
+                {"b",    value_type::func_name},
+                {"i",    value_type::func_name},
+                {"1",    value_type::number},
+                {"b",    value_type::func_name},
+                {"i",    value_type::func_name},
+                {"2",    value_type::number},
+                {"c",    value_type::func_name},
+                {"2",    value_type::number},
+                {"2",    value_type::number}
+        }));
+    }
 
 BOOST_AUTO_TEST_SUITE_END()
